58cf7ec940720907e141af37b87c88ce90dbd4b5
Zero-definition <saathvikchoudhary@gmail.com>
2024-03-22 11:47:00 +0530

    minor changes

--- src/main/java/org/example/bmi_calculator/BMI_Calculator.java
+++ src/main/java/org/example/bmi_calculator/BMI_Calculator.java
@@ -11,12 +11,10 @@ public class BMI_Calculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BMI_Calculator.class.getResource("BMI_frontend.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 322, 592);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("BMI Calculator by Saathvik");
         stage.setScene(scene);
         stage.show();
-
-
     }
 
     public static void main(String[] args) {
