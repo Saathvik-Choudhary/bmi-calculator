2dd4ced2df1abb7914d05394c6be6384ef602b09
Zero-definition <saathvikchoudhary@gmail.com>
2024-03-26 16:45:16 +0530

    Creating Tests

---
+++ src/test/java/org/example/bmi_calculator/BMIControllerTest.java
@@ -0,0 +1,32 @@
+package org.example.bmi_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BMIControllerTest {
+
+    @Test
+    public void BMITestsinInchesKg()
+    {
+
+    }
+
+    @Test
+    public void BMITestsinInchesG()
+    {
+
+    }
+
+    @Test
+    public void BMITestsinCmKg()
+    {
+
+    }
+
+    @Test
+    public void BMITestsinCMg()
+    {
+
+    }
+}
